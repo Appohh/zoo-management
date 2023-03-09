0a933bdd31545998225006f8c715c21e3fc0086a
Jarno Dijkmans <513745@student.fontys.nl>
2023-03-09 11:58:27 +0100

    Added first code design

---
+++ Zoo_Bazaar/Zoo_Bazaar_forms/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Zoo_Bazaar_forms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lbx_test = new System.Windows.Forms.ListBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.lb_current_Employees = new System.Windows.Forms.Label();
+			this.lb_function = new System.Windows.Forms.Label();
+			this.btn_create_employee = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.lb_More_details = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lbx_Employees = new System.Windows.Forms.ListBox();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lbx_test
+			// 
+			this.lbx_test.BackColor = System.Drawing.Color.Black;
+			this.lbx_test.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbx_test.ForeColor = System.Drawing.Color.White;
+			this.lbx_test.FormattingEnabled = true;
+			this.lbx_test.ItemHeight = 40;
+			this.lbx_test.Location = new System.Drawing.Point(46, 258);
+			this.lbx_test.Name = "lbx_test";
+			this.lbx_test.Size = new System.Drawing.Size(1818, 84);
+			this.lbx_test.TabIndex = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+			this.panel1.Controls.Add(this.lb_current_Employees);
+			this.panel1.Controls.Add(this.lb_function);
+			this.panel1.Controls.Add(this.btn_create_employee);
+			this.panel1.Location = new System.Drawing.Point(46, 40);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1818, 193);
+			this.panel1.TabIndex = 2;
+			// 
+			// lb_current_Employees
+			// 
+			this.lb_current_Employees.AutoSize = true;
+			this.lb_current_Employees.ForeColor = System.Drawing.Color.White;
+			this.lb_current_Employees.Location = new System.Drawing.Point(29, 76);
+			this.lb_current_Employees.Name = "lb_current_Employees";
+			this.lb_current_Employees.Size = new System.Drawing.Size(0, 40);
+			this.lb_current_Employees.TabIndex = 2;
+			// 
+			// lb_function
+			// 
+			this.lb_function.AutoSize = true;
+			this.lb_function.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_function.ForeColor = System.Drawing.Color.White;
+			this.lb_function.Location = new System.Drawing.Point(27, 23);
+			this.lb_function.Name = "lb_function";
+			this.lb_function.Size = new System.Drawing.Size(229, 40);
+			this.lb_function.TabIndex = 1;
+			this.lb_function.Text = "Administrator";
+			// 
+			// btn_create_employee
+			// 
+			this.btn_create_employee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(66)))), ((int)(((byte)(76)))));
+			this.btn_create_employee.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_create_employee.ForeColor = System.Drawing.Color.White;
+			this.btn_create_employee.Location = new System.Drawing.Point(1399, 97);
+			this.btn_create_employee.Name = "btn_create_employee";
+			this.btn_create_employee.Size = new System.Drawing.Size(383, 72);
+			this.btn_create_employee.TabIndex = 0;
+			this.btn_create_employee.Text = "Create New Employee";
+			this.btn_create_employee.UseVisualStyleBackColor = false;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.Black;
+			this.panel2.Controls.Add(this.lb_More_details);
+			this.panel2.Controls.Add(this.panel3);
+			this.panel2.Location = new System.Drawing.Point(1932, 137);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(649, 1112);
+			this.panel2.TabIndex = 3;
+			// 
+			// lb_More_details
+			// 
+			this.lb_More_details.AutoSize = true;
+			this.lb_More_details.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_More_details.ForeColor = System.Drawing.Color.White;
+			this.lb_More_details.Location = new System.Drawing.Point(195, 1010);
+			this.lb_More_details.Name = "lb_More_details";
+			this.lb_More_details.Size = new System.Drawing.Size(260, 48);
+			this.lb_More_details.TabIndex = 1;
+			this.lb_More_details.Text = "More details";
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.White;
+			this.panel3.Location = new System.Drawing.Point(57, 184);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(536, 10);
+			this.panel3.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Silver;
+			this.label1.Location = new System.Drawing.Point(2048, 82);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(396, 40);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Selected Employee Info";
+			// 
+			// lbx_Employees
+			// 
+			this.lbx_Employees.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+			this.lbx_Employees.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbx_Employees.ForeColor = System.Drawing.Color.White;
+			this.lbx_Employees.FormattingEnabled = true;
+			this.lbx_Employees.ItemHeight = 38;
+			this.lbx_Employees.Location = new System.Drawing.Point(46, 321);
+			this.lbx_Employees.Name = "lbx_Employees";
+			this.lbx_Employees.Size = new System.Drawing.Size(1818, 954);
+			this.lbx_Employees.TabIndex = 0;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(54, 53);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(2573, 1386);
+			this.tabControl1.TabIndex = 5;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(74)))), ((int)(((byte)(29)))));
+			this.tabPage1.Controls.Add(this.panel1);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.lbx_Employees);
+			this.tabPage1.Controls.Add(this.panel2);
+			this.tabPage1.Controls.Add(this.lbx_test);
+			this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tabPage1.Location = new System.Drawing.Point(10, 47);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(2553, 1329);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Employee management";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(10, 47);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(2553, 1329);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "AnimalManagement";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(2624, 1436);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ListBox lbx_test;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label lb_current_Employees;
+		private System.Windows.Forms.Label lb_function;
+		private System.Windows.Forms.Button btn_create_employee;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label lb_More_details;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox lbx_Employees;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+	}
+}
+
---
+++ Zoo_Bazaar/Zoo_Bazaar_forms/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Zoo_Bazaar_forms
+{
+	public partial class Form1 : Form
+	{
+		String stdDetails = "{0, -15}{1, -20}{2, -20}{3, -20}{4, -20}{5, -20}{6, -20}";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.Size = new Size(1180, 650);
+			lbx_test.Items.Add(String.Format(stdDetails, "Profile","FirstName","LastName","Email","Job","Phone","Status"));
+
+		}
+	}
+}
