e7db332c928d23be5b9caed99e5990b5955a8bd6
Nguyen Ngo <491567@student.fontys.nl>
2023-03-16 11:00:28 +0100

    Added Species and Type

---
+++ Application/Zoo_Bazaar/Logic/Species.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCL
+{
+    public class Species
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Species(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
