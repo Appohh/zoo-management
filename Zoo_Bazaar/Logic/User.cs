0f1e5293f688926cd580d8fb4355c1ba19731a94
Jarno Dijkmans <513745@student.fontys.nl>
2023-03-16 10:35:27 +0100

    Changed code

---
+++ Zoo_Bazaar/Logic/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    abstract class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string BirthDate { get; set; }
+        public string address { get; set; }
+        public string City { get; set; }
+
+        public User (int id, string firstName, string surName, string userName, string password, string email)
+        {
+
+        }
+
+
+        public void login()
+        {
+
+        }
+    }
+}
