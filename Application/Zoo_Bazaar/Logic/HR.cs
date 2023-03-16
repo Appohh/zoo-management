73e98cc0cb4ebbe8d1aac543a8d96d17ed904b2d
Jarno Dijkmans <513745@student.fontys.nl>
2023-03-16 12:58:15 +0100

    Updated Employees and HR

---
+++ Application/Zoo_Bazaar/Logic/HR.cs
@@ -0,0 +1,19 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCL
+{
+    public class HR : Employees
+    {
+        public HR(string SpouseName, int SpousePhone, string EmergencyName, int EmergencyPhone, int BSN, int Contractstatus, int id, string firstName, string surName, string userName, string password, string email) : base(SpouseName, SpousePhone, EmergencyName, EmergencyPhone, BSN, Contractstatus, id, firstName, surName, userName, password, email)
+        {
+            
+        }
+
+        
+    }
+}
