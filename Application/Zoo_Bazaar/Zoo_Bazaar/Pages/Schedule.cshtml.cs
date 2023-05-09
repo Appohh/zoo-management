6fade7450c1a34a378a71193c47e73b18610c8cb
Appie_pc <alpay2001@live.nl>
2023-05-09 19:32:45 +0200

    schedule html css base made, working on javascript to bind events to schedule.

--- Application/Zoo_Bazaar/Zoo_Bazaar/Pages/Schedule.cshtml.cs
+++ Application/Zoo_Bazaar/Zoo_Bazaar/Pages/Schedule.cshtml.cs
@@ -1,12 +1,45 @@
+using LogicCL;
+using LogicCL.Repository;
+using LogicCL.Users;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Zoo_Bazaar.Pages
 {
     public class ScheduleModel : PageModel
     {
+        public User? LoggedEmployee { get; set; }
+        private EmployeeRepository? _employeeRepository { get; set; }
+
+        public WeekSchedule currentWeekSchedule;
+        public List<Shift> loggedEmployeeShifts { get; set; }
+        public string scheduleJson = "";
+
+        public ScheduleModel() {
+            _employeeRepository = new EmployeeRepository();
+            currentWeekSchedule = new WeekSchedule(DateTime.Now);
+        }
+
         public void OnGet()
         {
+            if (_employeeRepository == null)
+            {
+                return;
+            }
+
+
+            var idClaim = User.FindFirst("id");
+            if (idClaim != null)
+            {
+                int idValue = Convert.ToInt32(idClaim.Value);
+                LoggedEmployee = _employeeRepository.getUserById(idValue);
+            }
+
+            if (LoggedEmployee == null) { return; }
+
+            loggedEmployeeShifts = _employeeRepository.GetShiftsByEmpId(LoggedEmployee.Id);
+            scheduleJson = JsonConvert.SerializeObject(loggedEmployeeShifts);
         }
     }
 }
