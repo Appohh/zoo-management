15d33b79688215ff1106ce743b78288929cef6fa
Jarno <513745@student.fontys.nl>
2023-06-16 16:58:24 +0200

    Changes to PaymentRepository

---
+++ Application/Zoo_Bazaar/Logic/Repository/PaymentRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCL.Repository
+{
+	public class PaymentRepository
+	{
+		//private Discount discount;
+		//private Ticket tickets; 
+
+		//public Payment ApplyDiscount(Payment payment, string code)
+		//{
+		//	if (payment.TicketIDs.Count <= 20)
+		//	{
+		//		if (tickets.Name == "TicketBabys")
+		//		{
+		//			//Change ticketprice 
+		//		}
+		//		else if (tickets.Name == "TicketKids")
+		//		{
+		//			//Change ticketprice
+		//		}
+		//		else if (tickets.Name == "TicketsAdults")
+		//		{
+		//			//Change ticketprice
+		//		}
+		//		//calculate new totalprice
+		//	}
+			
+		//	if (discount.Code == code)
+		//	{s
+		//		//Calculate new price 
+		//	}
+
+		//	return payment;
+
+		//}
+	}
+}
