658ef7770f2480a9d0e2c5d6467da87326e4b918
solaalec <sola.alec@gmail.com>
2023-06-16 15:02:18 +0200

    ticket

---
+++ Application/Zoo_Bazaar/Data/DTOs/TicketDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCL.DTOs
+{
+    public class TicketDTO
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public TicketDTO(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+        public TicketDTO()
+        {
+
+        }
+     
+    }
+}
