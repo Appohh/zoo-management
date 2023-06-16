40eb93d67c0d7764a39c66b5813d9dc7019e7c2d
solaalec <sola.alec@gmail.com>
2023-06-16 15:36:35 +0200

    payment

---
+++ Application/Zoo_Bazaar/Data/DTOs/PaymentDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCL.DTOs
+{
+    public class PaymentDTO
+    {
+        public int Id { get; private set; }
+        public int CustomerId { get; private set; }
+        public List<int> TicketId { get; private set; }
+
+        public PaymentDTO(int id, int customerId, List<int> ticketId)
+        {
+            Id = id;
+            CustomerId = customerId;
+            TicketId = ticketId;
+        }
+    }
+}
---
+++ Application/Zoo_Bazaar/Logic/Payment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCL
+{
+    public class Payment
+    {
+        public int Id { get; private set; }
+        public int CustomerId { get; private set; }
+        public List<int> TicketId { get; private set; }
+
+        public Payment(int id, int customerId, List<int> ticketId)
+        {
+            Id = id;
+            CustomerId = customerId;
+            TicketId = ticketId;
+        }
+    }
+}
