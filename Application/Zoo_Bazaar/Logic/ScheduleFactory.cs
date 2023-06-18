ab09a45250c522a5427c773809297a1c45381808
Nguyen Ngo <491567@student.fontys.nl>
2023-06-18 20:49:33 +0200

    update

---
+++ Application/Zoo_Bazaar/Logic/ScheduleFactory.cs
@@ -0,0 +1,21 @@
+﻿using DataCL.DataTraffic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCL
+{
+    public class ScheduleFactory
+    {
+        private UserDataTraffic userDataTraffic = new UserDataTraffic();
+        private JobDataTraffic jobDataTraffic = new JobDataTraffic();
+        private ShiftDataTraffic shiftDataTraffic = new ShiftDataTraffic();
+        private LocationDataTraffic locationDataTraffic = new LocationDataTraffic();
+        private AbsenceDataTraffic absenceDataTraffic = new AbsenceDataTraffic();
+        private AbsenceTypeDataTraffic absenceTypeDataTraffic = new AbsenceTypeDataTraffic();
+
+
+    }
+}
