4076eec22e4ad599805429875b9fffbe75e60713
solaalec <sola.alec@gmail.com>
2023-06-20 18:42:25 +0200

    orderDTO

---
+++ Application/Zoo_Bazaar/Data/DTOs/OrderDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCL.DTOs
+{
+    public class OrderDTO
+    {
+        public int? Id { get; set; }
+        public List<Tuple<int, int>> Tickets { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public int Paid { get; set; }
+
+        public OrderDTO(int? id, List<Tuple<int, int>> tickets, string name, string email, string phoneNumber, decimal? totalPrice, int paid)
+        {
+            Id = id;
+            Tickets = tickets;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            TotalPrice = totalPrice;
+            Paid = paid;
+        }
+        public OrderDTO()
+        {
+
+        }
+    }
+}
