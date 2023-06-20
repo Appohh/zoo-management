3bdd987cd63c9b7e1333e5cfcb51e2ce880fff6b
Appie <alpay2001@live.nl>
2023-06-20 17:05:09 +0200

    orders

--- Application/Zoo_Bazaar/Zoo_Bazaar/Pages/Tickets.cshtml.cs
+++ Application/Zoo_Bazaar/Zoo_Bazaar/Pages/Tickets.cshtml.cs
@@ -11,7 +11,7 @@ namespace Zoo_Bazaar.Pages
         public List<Ticket>? tickets { get; set; }
 
         [BindProperty]
-        public Payment Order { get; set; }
+        public Order Order { get; set; }
 
         public TicketsModel()
         {
@@ -21,6 +21,32 @@ namespace Zoo_Bazaar.Pages
 
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPostCheckout()
+        {
+
+            List<Tuple<int, int>> tickets_ = new List<Tuple<int, int>>();
+
+            int count = tickets_.Sum(t => t.Item2);
+
+            foreach (string key in Request.Form.Keys)
+            {
+                if (key.StartsWith("row"))
+                {
+                    string id = key.Replace("row", "");
+                    tickets_.Add(Tuple.Create(Convert.ToInt32(id.ToString()), Convert.ToInt32(Request.Form[key])));
+                }
+                else continue;
+            }
+
+            Order.Tickets = tickets_;
+
+
+
+
+            return Page();
 
         }
     }
